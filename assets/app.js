08e4ab1358e937cd5decdac49cfa29fc7ac5f958
Sebastião Dias Lutonda <lutonda@gmail.com>
2022-02-09 21:52:07 +0000

    fixed templating

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,8 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './front/css2.css';
+import './front/animate.css';
 
 // start the Stimulus application
 import './bootstrap';
