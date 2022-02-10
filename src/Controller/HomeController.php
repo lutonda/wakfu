72c2330b56104da4b718f600e2397580a343a181
Sebastião Dias Lutonda <lutonda@gmail.com>
2022-02-10 21:48:57 +0000

    start page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,23 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
+            $cursos=[
+                ['Tecnologia de Transformação Agroalimentar','Ciências Agrárias',5,5,200],
+                ['Agronomia','Ciências Agrárias',4,3,200],
+                ['Zootecnia','Ciências Agrárias',4,5,200],
+                
+                ['Engenharia Mecânica','Engenharia e Tecnologia',5,5,200],
+                ['Engenharia Industrial','Engenharia e Tecnologia',4,5,200],
+                ['Energia e Instalações Elétricas','Engenharia e Tecnologia',4,5,200],
+                
+                ['Gestão e Negócios','Administração e Negócios',4,5,200],
+                ['Gestão Industrial','Administração e Negócios',3,5,200],
+                
+            ];
         return $this->render('home/index.html.twig', [
+
             'controller_name' => 'HomeController',
+            'cursos'=>$cursos
         ]);
     }
 }
